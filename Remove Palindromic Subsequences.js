54b7a1dc1d31e4f961aa19e6f1094accb2e8a9b4
s.tcukanov@gmail.com <s.tcukanov@gmail.com>
2020-06-30 22:44:34 -0400

    +

---
+++ Remove Palindromic Subsequences.js
@@ -0,0 +1,20 @@
+/**
+https://leetcode.com/problems/remove-palindromic-subsequences/submissions/
+ */
+var removePalindromeSub = function(s) {
+ if (s.length === 0) return 0;
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] === s[right]) {
+            left++;
+            right--;
+        } else {
+            return 2;
+        }
+    }
+
+    return 1;
+}
